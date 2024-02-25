94d07388f8053b32cfb157cae85b87cc3568983c
Alexander Horner <33007665+alexhorner@users.noreply.github.com>
2024-02-25 22:58:03 +0000

    Implement basic leaderboards

--- CutelPhoneGame.Web/Controllers/LeaderboardController.cs
+++ CutelPhoneGame.Web/Controllers/LeaderboardController.cs
@@ -1,19 +1,43 @@
-﻿using CutelPhoneGame.Web.Authentication.Attributes;
+﻿using CutelPhoneGame.Core.Models;
+using CutelPhoneGame.Core.Providers;
+using CutelPhoneGame.Web.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CutelPhoneGame.Web.Controllers
 {
-    public class LeaderboardController : Controller
+    public class LeaderboardController(IPlayerProvider playerProvider) : Controller
     {
-        [AuthenticatedOnly]
-        public async Task<IActionResult> Index()
+        [HttpGet]
+        public async Task<IActionResult> Index([FromQuery] int? page)
         {
-            return View();
+            page ??= 0;
+            
+            if (page < 0) page = 0;
+
+            (List<PlayerModel> Players, PaginationModel Pagination) paginatedUsers = await playerProvider.GetAllPaginatedAsync(page.Value, orderByLeaderboard: true);
+            
+            return View(new BasicLeaderboardViewModel
+            {
+                Players = paginatedUsers.Players,
+                Pagination = new PaginatorPartialViewModel
+                {
+                    CurrentPage = paginatedUsers.Pagination.CurrentPage,
+                    MaxPage = paginatedUsers.Pagination.MaxPage,
+                    PageSwitchController = nameof(LeaderboardController),
+                    PageSwitchAction = nameof(Index)
+                }
+            });
         }
         
+        [HttpGet]
         public async Task<IActionResult> Public()
         {
-            return View();
+            (List<PlayerModel> Players, PaginationModel Pagination) paginatedUsers = await playerProvider.GetAllPaginatedAsync(0, 10, true);
+            
+            return View(new PublicBasicLeaderboardViewModel
+            {
+                Players = paginatedUsers.Players
+            });
         }
     }
 }
---
+++ CutelPhoneGame.Web/Models/PublicBasicLeaderboardViewModel.cs
@@ -0,0 +1,9 @@
+﻿using CutelPhoneGame.Core.Models;
+
+namespace CutelPhoneGame.Web.Models
+{
+    public class PublicBasicLeaderboardViewModel
+    {
+        public List<PlayerModel> Players { get; set; } = null!;
+    }
+}
