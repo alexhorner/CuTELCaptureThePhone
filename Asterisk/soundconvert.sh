87985a268c83e91c672687d9752ee01f261009de
Alexander Horner <33007665+alexhorner@users.noreply.github.com>
2024-04-15 22:48:32 +0100

    More sounds

--- Asterisk/soundconvert.sh
+++ Asterisk/soundconvert.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 rm -rf ./sounds
-find ./rawsounds -type f -name "*.mp3" -exec bash -c 'f="{}"; out="./sounds/${f#./rawsounds/}"; mkdir -p "$(dirname "$out")"; lame --decode "$f" "${out%.*}.wav" && sox "${out%.*}.wav" "${out%.*}.wav" gain 5 rate 8000' \;
+find ./rawsounds -type f -name "*.mp3" -exec bash -c 'f="{}"; out="./sounds/${f#./rawsounds/}"; tmp_out="${out%.*}_tmp.wav"; mkdir -p "$(dirname "$out")" && lame --decode "$f" "${out%.*}.wav" && sox "${out%.*}.wav" "$tmp_out" gain 5 rate 8000 && mv "$tmp_out" "${out%.*}.wav" || echo "Error processing $f"' \;
